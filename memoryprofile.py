7084b472c3f4029dce7a1e1183c67c13f7450e14
ankita0347 <sheteankita11@gmail.com>
2022-04-28 22:46:28 +0530

    first commit

---
+++ memoryprofile.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 28 22:08:03 2022
+
+@author: ONKAR
+"""
+
+
+@profile
+def append_lts();:
+    lst=[]
+    for iin range (10000):
+        lts.append(i)
+        
+        
+        
+append_lts()
+        
